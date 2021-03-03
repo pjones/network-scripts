4eea5f9893150e006680b7ce91d723e06e1ad36c
Peter Jones <pjones@devalot.com>
2021-03-03 10:47:27 -0700

    Add new script (vnc-via-ssh) and use makeWrapper instead of a pure shell

--- default.nix
+++ default.nix
@@ -1,13 +1,33 @@
 { pkgs ? import <nixpkgs> { }
 }:
+let
+  lib = pkgs.lib;
+  stdenv = pkgs.stdenvNoCC;
 
-pkgs.stdenvNoCC.mkDerivation {
-  name = "zshrc";
+  deps = with pkgs; [
+    openssh
+    sshfs
+  ];
+
+  path = lib.makeBinPath deps;
+in
+stdenv.mkDerivation {
+  name = "network-scripts";
+  meta.description = "Peter's networking scripts";
+
   phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+  buildInputs = deps ++ [ pkgs.makeWrapper ];
   src = ./.;
 
   installPhase = ''
-    mkdir -p $out/bin
-    install -m 0555 bin/sshfs $out/bin/sshfs
+    mkdir -p "$out/bin" "$out/wrapped"
+
+    for file in bin/*; do
+      name=$(basename "$file")
+      install -m 0555 "$file" "$out/wrapped"
+
+      makeWrapper "$out/wrapped/$name" "$out/bin/$name" \
+        --prefix PATH : "${path}"
+    done
   '';
 }
